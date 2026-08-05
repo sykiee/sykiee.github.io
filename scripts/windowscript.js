a528144bd4cea73df226db11c402dc3da167d527
sykiee <majorly08@gmail.com>
2026-08-04 20:05:07 -0500

    commit

--- scripts/windowscript.js
+++ scripts/windowscript.js
@@ -4,19 +4,16 @@ let activeContainer = null;
 function toggleContainer(id) {
     const selected = document.getElementById(id);
 
-    // If the clicked container is already open, close it
     if (activeContainer === selected) {
         selected.classList.remove("show");
         activeContainer = null;
         return;
     }
 
-    // Close all containers
     containers.forEach(container => {
         container.classList.remove("show");
     });
 
-    // Open the selected one
     selected.classList.add("show");
     activeContainer = selected;
 }
