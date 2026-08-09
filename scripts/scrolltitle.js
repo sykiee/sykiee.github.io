f4550713aeb750ecd44d3aedb5dad6c835ff03f9
sykiee <majorly08@gmail.com>
2026-08-09 15:13:55 -0500

    commit

---
+++ scripts/scrolltitle.js
@@ -0,0 +1,8 @@
+msg = "";
+msg = "bruce " + msg;position = 0;
+function scrolltitle() {
+document.title = msg.substring(position, msg.length) + msg.substring(0, position); position++;
+if (position > msg.length) position = 0
+window.setTimeout("scrolltitle()",250);
+}
+scrolltitle();
